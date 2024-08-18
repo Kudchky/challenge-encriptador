9ad438489d205f9f12786e89886c9d486d178681
Pol Castillo <rpcastilloy@gmail.com>
2024-08-18 18:50:03 -0500

    finish project

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-let inputText = document.getElementById("textArea");
+const inputText = document.getElementById("textArea");
+const indication = document.getElementById("indication");
+const result = document.getElementById("result");
 
 const vowels = {
     a: "ai",
@@ -44,41 +46,77 @@ const decrypt = (text) => {
     return newText;
 };
 
-const startEncrypting = () => {
-    let resultEncrypt = encrypt(inputText.value);
-
-    document.getElementById(
-        "result"
-    ).innerHTML = `<p class="text-result">${resultEncrypt}</p>
-                   <button class="button button__decrypt">Copy</button>`;
-
-    inputText.value = "";
+const warning = (
+    message = "Only lowercase letters and no accents",
+    color = false
+) => {
+    if (!color) {
+        indication.innerHTML = `<img src="./assets/exclamation.png" style="opacity: 0"/>
+                                <p>${message}</p>`;
+    } else {
+        indication.innerHTML = `<img src="../assets/warning.png"/>
+                                <p style="opacity: 0.7; color: var(--red)">${message}</p>`;
+    }
 };
 
-const startDecrypting = () => {
-    let resultDecrypt = decrypt(inputText.value);
+const resultNoFound = () => {
+    result.innerHTML = `<img src="./assets/doll.png" alt="" />
+                        <h3 class="message__title">No message was found</h3>
+                        <p class="message__text">Enter the text you want to encrypt or decrypt</p`;
+};
 
-    document.getElementById(
-        "result"
-    ).innerHTML = `<p class="text-result">${resultDecrypt}</p>
-                   <button class="button button__decrypt">Copy</button>`;
-
-    inputText.value = "";
+const resultEncrDecr = (resultEnDe) => {
+    result.innerHTML = `<p id="result-copy" class="text-result">${resultEnDe}</p>
+                        <button class="button button__copy" onclick="copy()">Copy</button>`;
 };
 
 const validateTextArea = () => {
-    const indication = document.getElementById("indication");
-
     const regex = /^[a-z\s]*$/;
 
     if (!regex.test(inputText.value)) {
-        indication.innerHTML = `<img src="../assets/warning.png"/>
-                                <p style="opacity: 0.7; color: var(--red)">Only lowercase letters and no accents</p>`;
+        warning("Only lowercase letters and no accents!", true);
         setTimeout(() => {
             inputText.value = inputText.value.replace(/[^a-z\s]/g, "");
         }, 300);
     } else {
-        indication.innerHTML = `<img src="./assets/exclamation.png" style="opacity: 0"/>
-                                <p>Only lowercase letters and no accents</p>`;
+        warning();
     }
+};
+
+const startEncrypting = () => {
+    const regex = /[aeiou]/;
+    if (inputText.value === "") {
+        warning("Enter text to ENCRYPT, do not leave this field empty!", true);
+    } else if (!regex.test(inputText.value))
+        warning("Enter at least one vowel!", true);
+    else {
+        let resultEncrypt = encrypt(inputText.value);
+        warning();
+        resultEncrDecr(resultEncrypt);
+    }
+};
+
+const startDecrypting = () => {
+    if (inputText.value === "") {
+        warning("Enter text to DECRYPT, do not leave this field empty!", true);
+    } else {
+        let resultDecrypt = decrypt(inputText.value);
+        if (inputText.value === resultDecrypt) {
+            warning("No encrypted messages were found!", true);
+            resultNoFound();
+        } else {
+            resultEncrDecr(resultDecrypt);
+        }
+    }
+};
+
+const reset = () => {
+    inputText.value = "";
+    warning();
+    resultNoFound();
+};
+
+const copy = () => {
+    inputText.value = "";
+    inputText.value = document.getElementById("result-copy").textContent;
 };
